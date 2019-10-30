7e5ed54a3816831527f28165c47b9c8e9f90c76e
Gabriel Anguiano <anguianog97@gmail.com>
2019-10-30 15:14:49 -0700

    useDarkMode custom hook set complete

---
+++ src/components/hooks/useDarkMode.jsx
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react'
+import { useLocalStorage } from './useLocalStorage'
+
+export const useDarkMode = (key, initialValue) => {
+    const [value, setValue] = useLocalStorage(key, initialValue);
+
+    const handleChange  = newValue => {
+        setValue(newValue)
+    }
+
+
+    useEffect(( ) => {
+        {value === true ? document.querySelector('body').classList.add('dark-mode') : document.querySelector('body').classList.remove('dark-mode')  }
+    })
+
+
+} 
+
+
+
+
+
